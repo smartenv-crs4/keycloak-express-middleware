a1592db966d9b32c100091a05de91fa22296afe8
Alessandro Romanino <a.romanino@gmail.com>
2025-11-06 19:25:52 +0100

    Fix protectMiddleware as Array of roles

--- index.js
+++ index.js
@@ -328,7 +328,7 @@ class keycloakExpressMiddleware {
         const self = this;
         return function(req, res, next){
             conditions = Array.isArray(conditions) ? conditions : [conditions];
-            return self.keycloak.protect((token) => {
+            self.keycloak.protect((token) => {
                 return conditions.some((role) => typeof role === 'string' && token.hasRole(role));
             })(req, res, next);
         }
